1fd5c7fdea0ff09fc6bddd41fd6446101da37ece
jonathan DiQuattro <jondiquattro@hotmail.com>
2022-09-11 09:37:33 -0400

    removed pem

--- dana-app/src/app/app.module.ts
+++ dana-app/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTabsModule } from '@angular/material/tabs';
+import {MatDivider} from '@angular/material/divider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './tabs/tabs.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeroComponent } from './hero/hero.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HeroComponent,
+    ContactMeComponent,
   ],
   imports: [
+    MatTabsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
