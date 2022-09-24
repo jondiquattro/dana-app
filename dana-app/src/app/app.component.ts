ad4b2521dfb0be1c5a04c9245294c5dcb77ab1bf
jonathan DiQuattro <jondiquattro@hotmail.com>
2022-09-24 16:19:56 -0400

    fuck my life

--- dana-app/src/app/app.component.ts
+++ dana-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { distinctUntilChanged, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dana-app';
+
+  Breakpoints = Breakpoints;
+  currentBreakpoint: string = '';
+
+  readonly breakpoint$ = this.breakpointObserver
+    .observe([Breakpoints.Large, Breakpoints.Medium, Breakpoints.Small, Breakpoints.HandsetPortrait, Breakpoints.HandsetLandscape])
+    .pipe(
+      distinctUntilChanged()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit(): void {
+    this.breakpoint$.subscribe(() =>
+      this.breakpointChanged()
+    );
+  }
+
+
+  private breakpointChanged() {
+    if (this.breakpointObserver.isMatched(Breakpoints.Large)) {
+      this.currentBreakpoint = Breakpoints.Large;
+    } else if (this.breakpointObserver.isMatched(Breakpoints.Medium)) {
+      this.currentBreakpoint = Breakpoints.Medium;
+    } else if (this.breakpointObserver.isMatched(Breakpoints.HandsetLandscape)) {
+      this.currentBreakpoint = Breakpoints.HandsetLandscape;
+    }
+    else if (this.breakpointObserver.isMatched(Breakpoints.HandsetPortrait)) {
+      this.currentBreakpoint = Breakpoints.HandsetPortrait;
+    }
+
+
+    // else if (this.breakpointObserver.isMatched('(min-width: 500px)')) {
+    //   this.currentBreakpoint = '(min-width: 500px)';
+    // }
+  }
 }
--- dana-app/src/app/tabs/tabs.component.ts
+++ dana-app/src/app/tabs/tabs.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { distinctUntilChanged, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent {
 
-  constructor() { }
+  align: string = 'center';
+  Breakpoints = Breakpoints;
+  currentBreakpoint: string = '';
+
+  readonly breakpoint$ = this.breakpointObserver
+    .observe([Breakpoints.Large, Breakpoints.Medium, Breakpoints.Small, Breakpoints.HandsetPortrait, Breakpoints.HandsetLandscape])
+    .pipe(
+      distinctUntilChanged()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
+    this.breakpoint$.subscribe(() =>
+      this.breakpointChanged()
+    );
+  }
+
+
+  private breakpointChanged() {
+    if (this.breakpointObserver.isMatched(Breakpoints.Large)) {
+      console.log('large')
+      this.currentBreakpoint = Breakpoints.Large;
+    } else if (this.breakpointObserver.isMatched(Breakpoints.Medium)) {
+      console.log('medium')
+      this.currentBreakpoint = Breakpoints.Medium;
+    } else if (this.breakpointObserver.isMatched(Breakpoints.HandsetLandscape)) {
+      console.log('landscape')
+      this.currentBreakpoint = Breakpoints.HandsetLandscape;
+    }
+    else if (this.breakpointObserver.isMatched(Breakpoints.HandsetPortrait)) {
+      console.log('portrait')
+      this.currentBreakpoint = Breakpoints.HandsetPortrait;
+    }
   }
 
 }
