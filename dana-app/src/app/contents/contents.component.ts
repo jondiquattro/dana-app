0d5a904a7e3e6c05ce5faa0d30e6b35674510fe7
jonathan DiQuattro <jondiquattro@hotmail.com>
2022-09-24 15:23:32 -0400

    added components

---
+++ dana-app/src/app/contents/contents.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnChanges } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-contents',
+  templateUrl: './contents.component.html',
+  styleUrls: ['./contents.component.css']
+})
+export class ContentsComponent {
+
+
+}
