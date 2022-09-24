a927427aa2447805c953487fa635f8adae48e79d
jonathan DiQuattro <jondiquattro@hotmail.com>
2022-09-24 15:12:20 -0400

    added reponsive controls

--- dana-app/src/scripts/deploy.sh
+++ dana-app/src/scripts/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scp -i ./dana.pem -r ../../dist ec2-user@ec2-54-210-179-97.compute-1.amazonaws.com:~/
+scp -i ./dana.pem -r ../../dist ec2-user@ec2-52-6-212-87.compute-1.amazonaws.com:~/
